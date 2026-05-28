95719f9ca584372e8013bcd4a0a4655418c69532
Gabriel Santos <gabriel.st@outlook.com>
2026-05-28 17:34:35 +0100

    fix: correct logo file extension from .PNG to .png

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from "react";
-import logo from "../../assets/logo4.PNG";
+import logo from "../../assets/logo4.png";
 
 export default function Header() {
   const [visible, setVisible] = useState(true);
